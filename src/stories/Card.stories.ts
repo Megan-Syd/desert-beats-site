4477c4e1020e32e48a099c58fb37609a5587bd9e
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-11 17:41:19 -0400

    adds more UI components to storybook for testing

---
+++ src/stories/Card.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import  CardContainer  from './Card';
+
+const meta = {
+  title: 'Example/CardContainer',
+  component: CardContainer,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  
+} satisfies Meta<typeof CardContainer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'Card Title',
+        subtitle: 'Card Subtitle',
+        children: 'This is card content',
+      },
+};
