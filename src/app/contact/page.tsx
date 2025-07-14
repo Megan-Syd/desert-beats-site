bd25308d72d07c557b47e20c854a8504b687eb18
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-14 15:53:09 -0400

    builds page layouts

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,8 @@
+import Card from "@/components/Card";
+import Footer from "@/components/Footer";
+import HeroImage from "@/components/HeroImage";
+import NavBar from "@/components/NavBar";
+import { Grid, Paper, Typography } from "@mui/material";
 import { Metadata } from "next";
 
 export const metadata : Metadata = {
@@ -7,7 +12,31 @@ export const metadata : Metadata = {
 export default function ContactPage() {
     return (
         <>
-        
+        <NavBar />
+        <HeroImage title="Contact Us" backgroundImageUrl="/banner_images/banner01.JPG" />
+        <Grid container spacing={2}>
+            <Grid size={{ xs: 6, md: 4}}>
+                <Card title={"Get In Touch"}>
+                    <Typography>96 33rd Avenue East<br />Saskatoon SK S7K 0S1</Typography>
+                    <Typography>melaniesydiaha@gmail.com</Typography>
+                    <Typography>(306)123-4567</Typography>
+                </Card>
+            </Grid>
+            <Grid size={{xs: 6, md: 8 }}>
+                <Paper sx={{ margin: '10px 10px 10px 0'}}>
+                <iframe
+                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2448.4578348319264!2d-106.66356392362759!3d52.144183071965074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5304f6e85e7e53f5%3A0xff23fa7b36940d06!2s96%20A%2033%20St%20E%2C%20Saskatoon%2C%20SK%20S7K%200S1!5e0!3m2!1sen!2sca!4v1752508594989!5m2!1sen!2sca"
+                        width="100%"
+                        height="300"
+                        style={{ border: 0, borderRadius: '5px' }}
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
+                </Paper>
+            </Grid>
+        </Grid>
+        <Footer />
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 import Image from "next/image";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import NavBar from "@/components/NavBar";
+import HeroImage from "@/components/HeroImage";
+import Card from "@/components/Card";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <Box>
-        <Container></Container>
-
-    </Box>
+    <>
+    <NavBar />
+    <HeroImage title="Desert Beats" backgroundImageUrl="/banner_images/banner02.jpg" />
+    <Container maxWidth='md'>
+      <Card title={"Welcome to Desert Beats"}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', // horizontal center
+        justifyContent: 'center', // vertical center (if needed)
+        minHeight: '100vh', // full viewport height
+        gap: 4,
+      }}>
+        <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla autem iusto et animi! Vitae impedit voluptatibus, nulla atque perspiciatis minus quos assumenda quis nesciunt quibusdam voluptas modi laborum, quod fugiat!</Typography>
+      </Card>
+      <Card title={"What's New?"}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', // horizontal center
+        justifyContent: 'center', // vertical center (if needed)
+        minHeight: '100vh', // full viewport height
+        gap: 4,
+      }}>
+        <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, rerum? Nihil adipisci repudiandae tempora, saepe excepturi facere hic rem. Inventore necessitatibus voluptas reprehenderit iste dolores earum perferendis consectetur ducimus fugiat?</Typography>
+      </Card>
+    </Container>
+    <Footer />
+    </>
       );
 }
