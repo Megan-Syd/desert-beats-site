4243d3cdad017a14ac7d3fba3301edb04a197476
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-15 14:35:40 -0400

    refactors layout, includes social media icon links

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -1,4 +1,5 @@
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Grid, Link, Typography } from "@mui/material";
+import { FaFacebook, FaYoutube } from "react-icons/fa";
 
 interface FooterLinksProps{
     title: string;
@@ -7,13 +8,39 @@ interface FooterLinksProps{
 
 export default function FooterLinks({title, links}: FooterLinksProps) {
     return(
-        <Box>
-            <Typography variant="h6">{title}</Typography>
-            {links.map(({label, href}) => (
-                <Typography key={label}>
-                    <Link href={href} color="inherit" underline="hover">{label}</Link>
-                </Typography>
-            ))}
-        </Box>
+        <Grid container spacing={2}>
+            <Grid size={{md: 9}}>
+                <Box>
+                    <Typography variant="h6">{title}</Typography>
+                    {links.map(({label, href}) => (
+                        <Typography key={label}>
+                            <Link href={href} color="inherit" underline="hover">{label}</Link>
+                        </Typography>
+                    ))}
+                </Box>
+            </Grid>
+            <Grid size={{md: 3}}>
+                <Box>
+                    <Typography variant="h5">
+                        <Link 
+                        href='#' 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        sx={{ color: 'whitesmoke'}}>
+                            <FaYoutube />
+                        </Link>
+                    </Typography>
+                    <Typography variant="h5">
+                        <Link 
+                        href='https://www.facebook.com/DesertBeatsBellyDance' 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        sx={{ color: 'whitesmoke'}}>
+                            <FaFacebook />
+                        </Link>
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
