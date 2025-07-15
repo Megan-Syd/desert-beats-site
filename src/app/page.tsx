e2726dbcc30f4dcd7851de94ae627e9ffd439664
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-15 18:11:20 -0400

    refactors to include new/updated components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import HeroImage from "@/components/HeroImage";
 import Card from "@/components/Card";
+import Headline from "@/components/Headline";
 
 export default function Home() {
   return (
     <>
-    <HeroImage title="Desert Beats" backgroundImageUrl="/banner_images/banner02.jpg" />
+    <HeroImage title="Desert Beats" buttonText="Register For Classes" backgroundImageUrl="/banner_images/banner02.jpg" />
     <Container maxWidth='md'>
       <Card title={"Welcome to Desert Beats"}
       sx={{
@@ -29,6 +30,13 @@ export default function Home() {
       }}>
         <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, rerum? Nihil adipisci repudiandae tempora, saepe excepturi facere hic rem. Inventore necessitatibus voluptas reprehenderit iste dolores earum perferendis consectetur ducimus fugiat?</Typography>
       </Card>
+      <Headline 
+      sx={{color: 'whitesmoke', padding: '40px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}
+      title={"Registrations are open!"} 
+      subtitle={"Click the link below to sign up"}>
+        <Button variant="outlined" color="inherit" sx={{ margin: '10px'}}>Register</Button>
+
+      </Headline>
     </Container>
     </>
       );
