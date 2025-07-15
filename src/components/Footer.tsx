9ed1d0ee0b12f9d2ada40215cc7417b2dc5958dc
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-15 13:47:07 -0400

    refactors layout style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Icon, Link, Stack, Typography } from "@mui/material";
+import { Box, Divider, Grid, Icon, Link, Stack, Typography } from "@mui/material";
 import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
@@ -6,18 +6,22 @@ export default function Footer() {
         <Box
         sx={{ backgroundColor: '#5e0435',
         color: 'whitesmoke',
-        padding: '30px'}}>
-            <Stack direction="row"
-            divider={<Divider orientation="vertical" flexItem />}
-            sx={{ justifyContent: 'space-evenly'}}
-            >
-                <Box>
+        padding: '30px',
+        width: '100%'}}>
+            <Grid container spacing={3}>
+                <Grid size={{ xs: 12, md: 4}}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
                     <Typography variant="h6">Desert Beats Belly Dance</Typography>
                     <Typography>96 A 33rd Street East</Typography>
                     <Typography>Saskatoon SK</Typography>
                     <Typography>S7K 0S1</Typography>
                 </Box>
-                <Box>                    
+                </Box>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4}}>
+                <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
                     <Typography variant="h6">Hours Of Operation</Typography>
                     <Typography>Monday</Typography>
                     <Typography>Tuesday</Typography>
@@ -27,7 +31,12 @@ export default function Footer() {
                     <Typography>Saturday</Typography>
                     <Typography>Sunday</Typography>
                     </Box>
-                <FooterLinks 
+                    </Box>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 4}}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
+                <FooterLinks  
                 title="Navigate"
                 links={[
                     { label: "Home", href: "/" },
@@ -37,7 +46,10 @@ export default function Footer() {
                     { label: "FAQs", href: "/faq" },
                     { label: "Contact", href: "/contact" },
                 ]}/>
-            </Stack>
+                </Box>
+                </Box>
+                </Grid>
+            </Grid>
 
         </Box>
     )
