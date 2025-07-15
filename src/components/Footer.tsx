eaa537fbab3c653d6967816141caaf4fdb846d4d
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-15 12:43:06 -0400

    inserts real text in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Icon, Stack, Typography } from "@mui/material";
+import { Box, Divider, Icon, Link, Stack, Typography } from "@mui/material";
+import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
     return(
@@ -11,17 +12,31 @@ export default function Footer() {
             sx={{ justifyContent: 'space-evenly'}}
             >
                 <Box>
-                    <Typography>Hello There</Typography>
-                    <Typography>How are you</Typography>
+                    <Typography variant="h6">Desert Beats Belly Dance</Typography>
+                    <Typography>96 A 33rd Street East</Typography>
+                    <Typography>Saskatoon SK</Typography>
+                    <Typography>S7K 0S1</Typography>
                 </Box>
                 <Box>                    
-                    <Typography>Hello There</Typography>
-                    <Typography>Hours Of Operation</Typography>
+                    <Typography variant="h6">Hours Of Operation</Typography>
+                    <Typography>Monday</Typography>
+                    <Typography>Tuesday</Typography>
+                    <Typography>Wednesday</Typography>
+                    <Typography>Thursday</Typography>
+                    <Typography>Friday</Typography>
+                    <Typography>Saturday</Typography>
+                    <Typography>Sunday</Typography>
                     </Box>
-                <Box>                    
-                    <Typography>Hello There</Typography>
-                    <Icon></Icon>
-                </Box>
+                <FooterLinks 
+                title="Navigate"
+                links={[
+                    { label: "Home", href: "/" },
+                    { label: "About Us", href: "/about" },
+                    { label: "Classes", href: "/classes" },
+                    { label: "Gallery", href: "/gallery" },
+                    { label: "FAQs", href: "/faq" },
+                    { label: "Contact", href: "/contact" },
+                ]}/>
             </Stack>
 
         </Box>
