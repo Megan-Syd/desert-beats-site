8ded6d106f48afbfb449869e5b456ec904a5d4db
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-16 17:40:02 -0400

    Adds basic UI elements to page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
+import Headline from "@/components/Headline";
+import HeroImage from "@/components/HeroImage";
+import { Card, CardContent, Container } from "@mui/material";
 import { Metadata } from "next";
 
 export const metadata : Metadata = {
@@ -7,6 +10,14 @@ export const metadata : Metadata = {
 export default function AboutPage() {
     return (
         <>
+            <HeroImage title="About Us" backgroundImageUrl="/banner_images/banner03.JPG"/>
+            <Container>
+                <Headline title={"Something something belly dance"} subtitle={"something else about dancey dance"} />
+
+                <Card>
+                    <CardContent></CardContent>
+                </Card>
+            </Container>
         
         </>
     )
