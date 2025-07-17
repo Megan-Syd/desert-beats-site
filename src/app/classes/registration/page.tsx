0b8fcbb7fd4cbee6da2961edf93b0656845c2e9f
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-17 16:10:51 -0400

    Adds hero image to registration page

--- src/app/classes/registration/page.tsx
+++ src/app/classes/registration/page.tsx
@@ -5,7 +5,7 @@ import { Container, Card, CardContent } from "@mui/material";
 export default function RegistrationPage() {
     return(
         <>
-            <HeroImage title="Register" backgroundImageUrl="#" />
+            <HeroImage title="Register" backgroundImageUrl="/banner_images/banner08.jpg" backgroundPosition="0 20%" />
 
             <Container>
                 <Headline title={"Registration is open from July to September"} subtitle={"Read below to see how it works"} />
