836787a0df71ec9c1fba61af1863d2dc42c4fd38
Meegun <134974590+Meegun@users.noreply.github.com>
2025-07-18 13:01:30 -0400

    Refactors to handle expanded state on AppAccordion

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,23 +1,57 @@
-import AppAccordion from "@/components/AppAccordion";
-import HeroImage from "@/components/HeroImage";
-import { Container } from "@mui/material";
-import { Metadata } from "next";
+'use client';
 
-export const metadata : Metadata = {
-    title: 'FAQ | Desert Beats',
-}
+import AppAccordion from "@/components/AppAccordion";
+import Headline from "@/components/Headline";
+import HeroImage from "@/components/HeroImage";
+import { Box, Button, Container } from "@mui/material";
+import { Metadata } from "next";
+import React from "react";
+
+// export const metadata : Metadata = {
+//     title: 'FAQ | Desert Beats',
+// }
 
 export default function FAQPage() {
+    const [expandedPanel, setExpandedPanel] = React.useState<number | false>(false);
+
+    const handleAccordionChange = (panelIndex: number) => (
+    event: React.SyntheticEvent,
+    isExpanded: boolean
+    ) => {
+    setExpandedPanel(isExpanded ? panelIndex : false);
+    };
+
+
     return (
         <>
             <HeroImage title="Frequently Asked Questions" backgroundImageUrl="/banner_images/banner06.jpeg" />
-            <Container 
-            maxWidth='sm'
-            sx={{
-                padding: 4
-            }}
-            >
-                <AppAccordion summary={""} children={undefined}></AppAccordion>
+            <Container maxWidth='sm'> 
+                <Box sx={{ padding: 4}}>
+                    {[
+                        {summary: 'Do I need to have prior dance experience?', children: 'lorem ipsum dolor sit amet' },
+                        {summary: 'What kind of clothing and footwear should I bring to class?', children: '' },
+                        {summary: 'When does the session begin?', children: '' },
+                        {summary: 'How do I know which class to choose?', children: '' },
+                        {summary: 'How do I sign up for classes?', children: '' },
+                        {summary: 'How do I find the studio?', children: '' },
+                        {summary: '7', children: '' },
+                        {summary: '8', children: '' },
+                    ].map((item, index) => (
+                        <AppAccordion 
+                            key={index} 
+                            summary={item.summary} 
+                            children={item.children}
+                            // sx={}
+                            expanded={expandedPanel === index}
+                            onChange={handleAccordionChange(index)}
+                        />
+                    ))}
+                </Box>
+                <Headline title={"Still have questions?"} subtitle={" Get in touch!"}>
+                    <Button href="/contact" variant='outlined' color="inherit" sx={{ margin: '10px'}}>
+                        Contact Us
+                    </Button>
+                </Headline>
             </Container>
         </>
     )
--- src/components/AppAccordion.tsx
+++ src/components/AppAccordion.tsx
@@ -1,17 +1,30 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, SxProps, Typography } from "@mui/material";
 import React, { ReactNode } from "react";
 import { MdExpandMore } from "react-icons/md";
 
 export interface AppAccordionProps{
     summary: string;
     children: ReactNode;
+    sx?: SxProps;
+    expanded?: boolean;
+    onChange?: (event: React.SyntheticEvent, isExpanded: boolean) => void;
 }
 
-export default function AppAccordion({summary, children}: AppAccordionProps) {
+export default function AppAccordion({summary, children, sx, expanded, onChange}: AppAccordionProps) {
     return(
-        <Accordion>
+        <Accordion 
+            expanded={expanded} 
+            onChange={onChange} 
+            sx={{ 
+                margin: 1, 
+                borderRadius: 1, 
+                // border: '5px solid transparent',
+                // '&.Mui-expanded': {
+                //     borderColor: '#226C3B'
+                // }
+            }}>
             <AccordionSummary
-            expandIcon={<MdExpandMore />}>
+            expandIcon={<MdExpandMore size={28} />}>
                 <Typography>{summary}</Typography>
             </AccordionSummary>
             <AccordionDetails>
