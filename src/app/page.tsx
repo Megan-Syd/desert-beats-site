24a730811ae070f61397c8ec014240ae2978e49c
Meegun <134974590+Meegun@users.noreply.github.com>
2025-08-06 23:23:52 -0600

    refactors to display newsData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, CardContent, CardHeader, CardMedia, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, CardContent, CardHeader, CardMedia, Container, Divider, Grid, Typography } from "@mui/material";
 import HeroImage from "@/components/HeroImage";
 import Card from "@/components/Card";
 import Headline from "@/components/Headline";
 import Link from "next/link";
+import newsData from "@/data/newsData";
 
 export default function Home() {
   return (
@@ -15,18 +16,8 @@ export default function Home() {
         subtitle={"movement - music - connection - cameraderie"}>
       </Headline>
 
-      {/* <Card title={"Welcome to Desert Beats"}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center', 
-        gap: 4,
-      }}>
-        <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla autem iusto et animi! Vitae impedit voluptatibus, nulla atque perspiciatis minus quos assumenda quis nesciunt quibusdam voluptas modi laborum, quod fugiat!</Typography>
-      </Card> */}
       <Card
-      title="What's New at Desert Beats"
+      // title={newsData.title}
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -34,20 +25,37 @@ export default function Home() {
         justifyContent: 'center',
         gap: 4,
       }}>
-          {/* <CardHeader title=""/> */}
-          <CardContent>
-            <Typography>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem, rerum? Nihil adipisci repudiandae tempora, saepe excepturi facere hic rem. Inventore necessitatibus voluptas reprehenderit iste dolores earum perferendis consectetur ducimus fugiat?
+        <Typography variant="h4">{newsData.title}</Typography>
+      {newsData.sections.map((section, index) => (
+        <CardContent key={index}>
+          {section.header && (
+            <Typography variant="h5">
+              {section.header}
             </Typography>
-            <CardMedia 
+            )}
+            {section.paragraphs.map((paragraph, i) => (
+              <Typography key={i}>
+                {paragraph}
+              </Typography>
+            ))}
+            {/* <CardMedia 
             component='img'
             image="/placeholder-square.png"
             sx={{padding: '100px'}}
+            /> */}
+            <Divider 
+              aria-hidden="true" 
+              sx={{
+                marginTop: 5, 
+                borderColor: '#226C3B'
+              }}
             />
         </CardContent>
+      ))}
       </Card>
+
       <Headline 
-      title={"Registrations are open!"} 
+      title={"Registration opening soon!"} 
       subtitle={"Click the link below to sign up"}>
         <Button href='/classes/registration' variant="outlined" color="inherit" sx={{ margin: '10px'}}>
           Register
