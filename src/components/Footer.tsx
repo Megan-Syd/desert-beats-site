341870dde3352e11dfe6cf8bf9238bd1f7ece2c7
Meegun <134974590+Meegun@users.noreply.github.com>
2025-09-19 15:20:31 -0400

    refactors layout for footer links and logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,43 +5,33 @@ import Image from "next/image";
 export default function Footer() {
     return(
         <Box
-        sx={{ backgroundColor: '#5e0435',
-        color: 'whitesmoke',
-        padding: '30px',
-        width: '100%'}}>
-            <Grid container spacing={3}>
-                <Grid size={{ xs: 12, md: 4}}>
-                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                        <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
-                            <Image src={"/dbbd-letters.svg"} alt={"Desert Beats Belly Dance letters logo"} height={200} width={200} />
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, md: 4}}>
-                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                        <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
-                            
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid size={{ xs: 12, md: 4}}>
-                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                        <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
-                            <FooterLinks  
-                            title="Navigate"
-                            links={[
-                                { label: "Home", href: "/" },
-                                { label: "About Us", href: "/about" },
-                                { label: "Classes", href: "/classes/descriptions" },
-                                { label: "Register", href: "/classes/registration"},
-                                { label: "Gallery", href: "/gallery" },
-                                { label: "FAQ", href: "/faq" },
-                                { label: "Contact", href: "/contact" },
-                            ]}/>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+            sx={{ 
+                backgroundColor: '#5e0435',
+                color: 'whitesmoke',
+                padding: '30px',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'space-around',
+                // flexWrap: 'wrap',
+                gap: 2
+            }}
+        >
+            <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
+                <Image src={"/dbbd-letters.svg"} alt={"Desert Beats Belly Dance letters logo"} height={200} width={200} />
+            </Box>
+            <Box sx={{ textAlign: 'left', maxWidth: '250px', width: '100%' }}>
+                <FooterLinks  
+                title="Navigate"
+                links={[
+                    { label: "Home", href: "/" },
+                    { label: "About Us", href: "/about" },
+                    { label: "Classes", href: "/classes/descriptions" },
+                    { label: "Register", href: "/classes/registration"},
+                    { label: "Gallery", href: "/gallery" },
+                    { label: "FAQ", href: "/faq" },
+                    { label: "Contact", href: "/contact" },
+                ]}/>
+            </Box>
         </Box>
     )
 }
